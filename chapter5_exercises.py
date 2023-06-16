d9f4b8d55333f634aed32b9403ecd3755412fde8
yorkeric <yorkeric@gmail.com>
2023-06-16 17:43:28 -0400

    pe q18

--- chapter5_exercises.py
+++ chapter5_exercises.py
@@ -1,14 +1,3 @@
-def is_prime(num):
-    for i in range(2, num):
-        if(num % i) == 0:
-            return False;
-    return True
-
-for i in range(1, 101):
-    if(is_prime(i)):
-        print(i)
-
-
 # Algorithm Workbench
 
 # Question 1
