d7e01c2eabb9cf13b572e0a9abc43f52d1422c9f
yorkeric <yorkeric@gmail.com>
2023-06-16 15:05:37 -0400

    Update chapter5_exercises.py

--- chapter5_exercises.py
+++ chapter5_exercises.py
@@ -2,6 +2,10 @@
 
 # Question 1
 # Write a function that accepts an argument and displays the argument multiplied by 10
+# delclar a function named times_ten and add print statment to display result
+# call function to display the result
+# 10 x 10 = 100
 def times_ten(num):
-    print(num)
-times_ten(1)
+    print(str(num) + " x 10 =", (num * 10))
+times_ten(10)
+
