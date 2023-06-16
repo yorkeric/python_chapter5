160be4f368c70416a70774e526f10185a27ee4ae
yorkeric <yorkeric@gmail.com>
2023-06-16 15:01:43 -0400

    initial commit of q1

---
+++ chapter5_exercises.py
@@ -0,0 +1,7 @@
+# Algorithm Workbench
+
+# Question 1
+# Write a function that accepts an argument and displays the argument multiplied by 10
+def times_ten(num):
+    print(num)
+times_ten(1)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+def times_ten(num):
+    print(num)
+    
+times_ten(1)
